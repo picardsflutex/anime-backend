d063f9a5b1e85a44d97c0dc0d40c20edaafa8ff0
picardsflutex <picardsflutex1@gmail.com>
2024-05-06 14:53:17 +0300

    feat: Api properties for anime

--- src/anime/anime.model.ts
+++ src/anime/anime.model.ts
@@ -1,4 +1,4 @@
-import { Table, Column, Model, DataType, HasMany, BelongsToMany, BelongsTo, ForeignKey } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, HasMany, BelongsToMany, BelongsTo, ForeignKey, IsArray } from 'sequelize-typescript';
 
 import { AnimeGenre } from 'src/genre/genre-anime.model';
 import { AnimeTag } from 'src/tag/tag-anime.model';
@@ -8,6 +8,8 @@ import { User } from 'src/users/users.model';
 import { Comment } from 'src/comment/comment.model';
 import { Genre } from 'src/genre/genre.model';
 import { Tag } from 'src/tag/tag.model';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 interface AnimeCreationAttrs{
   englishTitle: string;
@@ -21,45 +23,110 @@ interface AnimeCreationAttrs{
 
 @Table({tableName:'animes'})
 export class AnimeTitle extends Model<AnimeTitle, AnimeCreationAttrs> {
+  
+  @ApiProperty({example: 1, description: 'Unique identifier'})
   @Column({type: DataType.INTEGER, unique: true, primaryKey: true, autoIncrement: true })
   anime_id: number;
 
+  @ApiProperty({example: "Englesh title", description: 'Title of anime'})
+  @IsString()
   @Column({type: DataType.STRING, unique: true, allowNull: false})
   englishTitle: string;
 
+  @ApiProperty({example: "Original title", description: 'Original title of anime'})
+  @IsString()
   @Column({type: DataType.STRING, unique: true})
   originalTitle: string;
 
+  @ApiProperty({example: 2024, description: 'Year of release'})
+  @IsNumber()
   @Column({type: DataType.INTEGER, allowNull: false})
   releaseYear: number;
 
+  @ApiProperty({example: [{
+    "genre_id": 1,
+    "name": "genre1",
+    "createdAt": "2024-05-06T09:55:10.290Z",
+    "updatedAt": "2024-05-06T09:55:10.290Z",
+    "AnimeGenre": {
+      "id": 1,
+      "anime_id": 1,
+      "genre_id": 1
+    }
+  },
+  {
+    "genre_id": 2,
+    "name": "genre2",
+    "createdAt": "2024-05-06T09:55:10.291Z",
+    "updatedAt": "2024-05-06T09:55:10.291Z",
+    "AnimeGenre": {
+      "id": 2,
+      "anime_id": 1,
+      "genre_id": 2
+    }
+  }], description: 'genres'})
   @BelongsToMany(() => Genre, () => AnimeGenre)
   genres: Genre[]
 
+  @ApiProperty({example: [{
+    "tag_id": 1,
+    "name": "tag1",
+    "createdAt": "2024-05-06T09:55:10.274Z",
+    "updatedAt": "2024-05-06T09:55:10.274Z",
+    "AnimeTag": {
+      "id": 1,
+      "anime_id": 1,
+      "tag_id": 1
+    }
+  },
+  {
+    "tag_id": 2,
+    "name": "tag2",
+    "createdAt": "2024-05-06T09:55:10.279Z",
+    "updatedAt": "2024-05-06T09:55:10.279Z",
+    "AnimeTag": {
+      "id": 2,
+      "anime_id": 1,
+      "tag_id": 2
+    }
+  }], description: 'tags'})
   @BelongsToMany(() => Tag, () => AnimeTag)
   tags: Tag[]
 
+  @ApiProperty({example: 12, description: 'Number of current episodes.'})
+  @IsNumber()
   @Column({type: DataType.INTEGER, defaultValue: 0})
   episodesCurrent: number;
 
+  @IsNumber()
+  @ApiProperty({example: 12, description: 'Total number of episodes.'})
   @Column({type: DataType.INTEGER, defaultValue: 1})
   episodesTotal: number;
 
+  @ApiProperty({example: 12, description: 'Rating'})
+  @IsNumber()
   @Column({type: DataType.DECIMAL(2, 2), defaultValue: 0})
   rating: number;
 
+  @ApiProperty({example: 'Description for anime title.', description: 'Description.'})
+  @IsString()
   @Column({type: DataType.STRING, allowNull: false})
   description: string;
 
+  @ApiProperty({example: 'sadsd-321wsada312-1231sada.jpg', description: 'Path to the image.'})
+  @IsString()
   @Column({type: DataType.STRING})
   imagePath: string;
 
+  @ApiProperty({example: 'Commnet for title', description: 'Comments.'})
   @HasMany(() => Comment)
   comments: Comment[];
 
   @BelongsToMany(() => AnimeTitle, () => AnimeRelation, 'animeId', 'relatedAnimeId')
   relatedAnimes: AnimeTitle[];
 
+  @ApiProperty({example: 1, description: 'Author id.'})
+  @IsNumber()
   @ForeignKey(() => User)
   @Column({type: DataType.INTEGER, allowNull: false})
   user_id: number;
--- src/common/dto/animeDTOs/create-anime.dto.ts
+++ src/common/dto/animeDTOs/create-anime.dto.ts
@@ -1,25 +1,32 @@
-import { Genre } from "src/genre/genre.model";
-import { Tag } from "src/tag/tag.model";
 import { IsInt, IsOptional, IsString, Max, Min } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateAnimeDto {
+  @ApiProperty({example: 1, description: 'Unique identifier', required: true})
   @IsString()
   readonly englishTitle: string;
+  @ApiProperty({example: "Original title", description: 'Original title of anime'})
   @IsString()
   @IsOptional()
   readonly originalTitle?: string;
+  @ApiProperty({example: 2024, description: 'Year of release', required: true})
   @IsInt()
   @Min(1950)
   @Max(2100)
   readonly releaseYear: number;
+  @ApiProperty({example: ['genre1', 'genre2'], description: 'genres', required: true})
   readonly genres: string[];
+  @ApiProperty({example: ['tag1', 'tag2'], description: 'tags', required: true})
   readonly tags: string[];
+  @ApiProperty({example: 12, description: 'Number of current episodes.'})
   @IsInt()
   @IsOptional()
   readonly episodesCurrent?: number;
+  @ApiProperty({example: 12, description: 'Total number of episodes.'})
   @IsInt()
   @IsOptional()
   readonly episodesTotal?: number;
+  @ApiProperty({example: 'Description for anime title.', description: 'Description.'})
   @IsString()
   readonly description: string;
 }
